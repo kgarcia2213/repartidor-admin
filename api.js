ca5d2358d9184e2b455a65629e954bd53a678764
kgarcia2213 <kenia.garcia2213@gmail.com>
2025-10-27 15:10:05 -0500

    Add files via upload

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 const API_BASE = "https://api-repartidor-tm.onrender.com";
 
 async function apiGet(endpoint) {
-  const res = await fetch(`${API_BASE}/${endpoint}`);
+  const res = await fetch(`${API_BASE}/api/${endpoint}`);
   return res.json();
 }
 
 async function apiPost(endpoint, data) {
-  const res = await fetch(`${API_BASE}/${endpoint}`, {
+  const res = await fetch(`${API_BASE}/api/${endpoint}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
